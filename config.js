d5b05888a9e9ce5fffd499fd63323ca68cd29397
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-03-23 18:34:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {	
 hostedBy: true, 
-prefix: "",
-OwnerID: "", 
-token: "",
-support: "",
+prefix: "g.",
+OwnerID: "596060641740193856", 
+token: "ODIzOTQxNjIxMDU3ODQ3MzI5.YFoJbg.1eVKxqTKjVL-_Qy9WaJUVSwD3mA",
+support: "https://discord.gg/bBPh3jrT",
 basiclang: "en",
-color: "",
+color: "RED",
 reaction: "🎉", 
 grole: "Giveaway Management", 
 
