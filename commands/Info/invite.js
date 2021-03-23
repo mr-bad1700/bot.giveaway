6efb3d89e5c1016eb70d8915cbd0dc913d04c6c6
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-03-23 18:50:07 +0300

    Update invite.js

--- commands/Info/invite.js
+++ commands/Info/invite.js
@@ -1,9 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 
-exports.run = async (client, message, args, lang) => {
-    let websiteURL = `http://managegift.ga`;
+exports.run = async (client, message, args, lang) => { 
     let inviteLink = `https://discord.com/api/oauth2/authorize?client_id=795367729284644944&permissions=268957760&scope=bot`;
-    let voteURL = `https://discordbots.org/bot/${client.user.id}/vote`;
     let supportURL = client.config.support
     if (args[0] == "copy")return message.channel.send(inviteLink);
 
@@ -11,7 +9,6 @@ exports.run = async (client, message, args, lang) => {
         .setAuthor(lang.invite.main)
         .setDescription(lang.invite.disc)
         .addField(lang.invite.inv, (`[Click Here](${inviteLink})`))
-        .addField(lang.invite.vote, (`[Click Here](${voteURL})`))
         .addField(lang.invite.sup, (`[Click Here](${supportURL})`))
         .setColor("RED"))
         
