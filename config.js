2656ddf557e6c9edef2600d9a6e2e6cdd248a174
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-04-13 21:13:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 hostedBy: true, 
 prefix: "m.",
 OwnerID: "596060641740193856", 
-token: "NzY0NDg3NDE2NzQ4MzEwNTcw.X4G-Yw.Dwgm9WcHzvnkaQ3IKNBDPxsIrMM",
+token: "NzY0NDg3NDE2NzQ4MzEwNTcw.X4G-Yw.0spctXCwJ5FOUsXCV7YLB638vd4",
 support: "https://discord.gg/Zhwg47uFun",
 basiclang: "en",
 color: "BLACK",
