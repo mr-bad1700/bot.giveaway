04e5f22f52def51f29e95a5585526b422b54d14f
Mr.BAD <79611127+mr-bad1700@users.noreply.github.com>
2021-04-24 23:29:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ OwnerID: "596060641740193856",
 token: "NzY0NDg3NDE2NzQ4MzEwNTcw.X4G-Yw.0spctXCwJ5FOUsXCV7YLB638vd4",
 support: "https://discord.gg/Zhwg47uFun",
 basiclang: "en",
-color: "BLACK",
+color: "#303135",
 reaction: "🎉", 
 grole: "Giveaway Management", 
 
